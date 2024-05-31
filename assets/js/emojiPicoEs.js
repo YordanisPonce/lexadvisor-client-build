a0ca5312a9552e2275a77da1403253710498c94f
Debian <bitnami@ip-172-26-13-0.eu-west-3.compute.internal>
2024-05-31 00:24:43 +0000

    Save changes

---
+++ assets/js/emojiPicoEs.js
@@ -0,0 +1,39 @@
+const _default = {
+    // Category names. If a translated category name is available in the category data, the values shown
+    // here take precendence. If you want to use the category name provided by Emojibase, you can remove the category 
+    // from the i18n bundle.
+    'categories.activities': 'Actividades y Eventos',
+    'categories.animals-nature': 'Animales y Naturaleza',
+    'categories.custom': 'Emojis personalizados',
+    'categories.flags': 'Banderas',
+    'categories.food-drink': 'Alimentación y Bebidas',
+    'categories.objects': 'Objetos',
+    'categories.people-body': 'Personas',
+    'categories.recents': 'Uso Frecuente',
+    'categories.smileys-emotion': 'Emoticonos y Emociones',
+    'categories.symbols': 'Simbolos',
+    'categories.travel-places': 'Viajes y Lugares',
+
+    // Shown if there is an error creating or accessing the local emoji database.
+    'error.load': 'Error al cargar emojis',
+
+    // Messages for the Recents category.
+    'recents.clear': 'Borrar emojis recientes',
+    'recents.none': 'Aún no has seleccionado ningún emoji.',
+
+    // A retry button shown on the error view.
+    'retry': 'Intentar otra vez',
+
+    // Tooltip/title for the clear search button in the search field.
+    'search.clear': 'Borrar búsqueda',
+
+    // Shown when there is an error searching the emoji database.
+    'search.error': 'Error al buscar emojis',
+
+    // Shown when no emojis match the search query.
+    'search.notFound': 'No se encontraron resultados',
+
+    // Placeholder for the search field.
+    'search': 'Buscar emojis...'
+};
+export default _default;
